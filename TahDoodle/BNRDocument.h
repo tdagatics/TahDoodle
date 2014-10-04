73fc580a8f8d4e68741655a062e7b5a815d99735
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-04 11:41:08 -0700

    Initial Commit

---
+++ TahDoodle/BNRDocument.h
@@ -0,0 +1,13 @@
+//
+//  BNRDocument.h
+//  TahDoodle
+//
+//  Created by Anthony Dagati on 10/4/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BNRDocument : NSDocument
+
+@end
---
+++ TahDoodle/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TahDoodle
+//
+//  Created by Anthony Dagati on 10/4/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
